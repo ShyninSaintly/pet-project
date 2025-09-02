6f4a9c09a71856511138433fa300e56c4c15b6a6
Gleb <mamaka.s.pistoletom@gmail.com>
2025-09-02 16:13:56 +0300

    Added use Droppable

--- my-uikit-app/src/pages/DeskPage/DeskPage.tsx
+++ my-uikit-app/src/pages/DeskPage/DeskPage.tsx
@@ -1,52 +1,71 @@
-import Table from 'react-bootstrap/Table';
-import Container from 'react-bootstrap/Container';
-import classes from './DeskPage.module.scss'
+// import Table from 'react-bootstrap/Table';
+// import Container from 'react-bootstrap/Container';
+// import classes from './DeskPage.module.scss'
+import {useState} from "react";
+import { DndContext } from "@dnd-kit/core";
+import {Draggable} from "../../shared/ui/dragAndDrop/Draggable.tsx";
+import { Droppable } from "../../shared/ui/dragAndDrop/Droppable.tsx";
+const [parent, setParent] = useState(null);
+const draggable = (
+    <Draggable id="draggable">
+        Go ahead, drag me.
+    </Draggable>
+);
 export const DeskPage = () => {
     return (
-
-        <Container fluid>
-            <Table responsive="xl" className={classes.DeskPageTable}>
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Table heading</th>
-                    <th>Table heading</th>
-                    <th>Table heading</th>
-                    <th>Table heading</th>
-                    <th>Table heading</th>
-                    <th>Table heading</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                </tr>
-                </tbody>
-            </Table>
-</Container>
+        <DndContext onDragEnd={handleDragEnd}>
+            {!parent ? draggable : null}
+            <Droppable id="droppable">
+                {parent === "droppable" ? draggable : 'Drop here'}
+            </Droppable>
+        </DndContext>
+//         <Container fluid>
+//             <Table responsive="xl" className={classes.DeskPageTable}>
+//                 <thead>
+//                 <tr>
+//                     <th>#</th>
+//                     <th>Table heading</th>
+//                     <th>Table heading</th>
+//                     <th>Table heading</th>
+//                     <th>Table heading</th>
+//                     <th>Table heading</th>
+//                     <th>Table heading</th>
+//                 </tr>
+//                 </thead>
+//                 <tbody>
+//                 <tr>
+//                     <td>1</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                 </tr>
+//                 <tr>
+//                     <td>2</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                 </tr>
+//                 <tr>
+//                     <td>3</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                     <td>Table cell</td>
+//                 </tr>
+//                 </tbody>
+//             </Table>
+// </Container>
     );
+    // @ts-ignore
+    function handleDragEnd({over}) {
+        setParent(over ? over.id : null);
+    }
 };
---
+++ my-uikit-app/src/shared/ui/dragAndDrop/Droppable.tsx
@@ -0,0 +1,18 @@
+
+import {useDroppable} from '@dnd-kit/core';
+
+// @ts-ignore
+export const Droppable=(props)=> {
+    const {isOver, setNodeRef} = useDroppable({
+        id: props.id,
+    });
+    const style = {
+        opacity: isOver ? 1 : 0.5,
+    };
+
+    return (
+        <div ref={setNodeRef} style={style}>
+            {props.children}
+        </div>
+    );
+}
