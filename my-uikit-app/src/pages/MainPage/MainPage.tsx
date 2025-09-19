b6b663f820d0bacf46083aa72fdda187011ed087
Gleb <mamaka.s.pistoletom@gmail.com>
2025-09-19 11:37:25 +0300

    Db fix

--- my-uikit-app/src/pages/MainPage/MainPage.tsx
+++ my-uikit-app/src/pages/MainPage/MainPage.tsx
@@ -2,8 +2,9 @@ import { Container } from 'uikit-react'
 import {NavLinks} from "../../shared/ui/NavLinks/NavLinks.tsx";
 export const MainPage = () => {
     return (
-        <><NavLinks/>
-        <Container marginTop='100px'>
+        <>
+        <NavLinks/>
+        <Container>
             <h1>Главная</h1>
             <li>
 
